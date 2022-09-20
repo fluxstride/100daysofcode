c1c6cefa8aee696cd6c6f53c123f72d04284d684
samuel <fluxstride@DESKTOP-17HDHC4.localdomain>
2022-09-20 17:06:46 +0100

    Worked with closures in python

--- Day_2/python_closures.py
+++ Day_2/python_closures.py
@@ -1,4 +1,5 @@
 def greet(name):
+    """A simple function illustrating closures"""
     text = "Welcome to the world of closures #100daysofcode."
 
     def inner_function():
@@ -11,4 +12,14 @@ greet_samuel = greet("Samuel")
 greet_daniel = greet("Daniel")
 
 print(greet_samuel())
+# closed values in greet_samuel
+print(f"closed value 1: {greet_samuel.__closure__[0].cell_contents}")
+print(f"closed value 2: {greet_samuel.__closure__[1].cell_contents}")
+print("*" * 50)
+
+
 print(greet_daniel())
+# closed values in greet_daniel
+print(f"closed value 1: {greet_daniel.__closure__[0].cell_contents}")
+print(f"closed value 2: {greet_daniel.__closure__[1].cell_contents}")
+print("*" * 50)
